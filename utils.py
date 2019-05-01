4575c6fa53e6626795dfd0e4237e3fa724333dcf
aditya-malte <aditya.malte@gmail.com>
2019-05-01 19:22:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,7 +18,10 @@ def preprocess(text):
             if replaced == processed_text:
                 break
             processed_text = replaced
-            
+    
+    for punctuation in string.punctuation:
+        processed_text =  processed_text.replace(punctuation, " " + punctuation)
+        
     #remove numbers
     processed_text = re.sub("\d+", "", processed_text)
     
@@ -32,5 +35,5 @@ def preprocess(text):
     return processed_text
 
 
-print(preprocess("Check out this123 @#url https://stackoverflow.com/questions/11331982/how-to-remove-any-url-within-a-string-in-python my car another urlhttps://codereview.stackexchange.com/questions/186614/text-cleaning-script-producing-lowercase-words-with-minimal-punctuation"))
+print(preprocess("Check out this123!!! @#u`??rl https://stackoverflow.com/questions/11331982/how-to-remove-any-url-within-a-string-in-python my car another urlhttps://codereview.stackexchange.com/questions/186614/text-cleaning-script-producing-lowercase-words-with-minimal-punctuation"))
 print(preprocess("Check out this @#url https://stackoverflow.com/questions/11331982/how-to-remove-any-url-within-a-string-in-pyth"))
