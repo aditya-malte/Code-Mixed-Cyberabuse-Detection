69f9207e2795078b96dd76955a3c152c0059af29
Aditya Malte <aditya.malte@gmail.com>
2019-09-01 20:58:31 +0530

    temporary change #2

--- utils.py
+++ utils.py
@@ -62,7 +62,7 @@ def preprocess(text):
     print(processed_text)
     """
     
-    return processed_text
+    return processed_text.lower()
 
 
 print(preprocess("CHECK @out this123!!! \n u`??rl https://stackoverflow.com/questions/11331982/how-to-remove-any-url-within-a-string-in-python my car another 😅urlhttps://codereview.stackexchange.com/questions/186614/text-cleaning-script-producing-lowercase-words-with-minimal-punctuation"))
